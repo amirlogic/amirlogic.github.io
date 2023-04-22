63db2b5250387c6955ad2f88735fa6310e620fc5
Amir Hachaichi <amir.hac@outlook.com>
2023-04-22 23:46:45 +0200

    added 3d cube

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 
-gsap.from(".navbar", {"y":-100})
-gsap.from(".celltxt", {"marginTop":"600px","marginBottom":"600px"})
+gsap.from(".navbar", {delay:1, "y":-100})
+gsap.from(".celltxt", {duration:1, "marginTop":"600px","marginBottom":"600px", ease:'power0.out'}) // , ease:'power0.out'
